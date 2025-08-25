42ef0802cbb949f828fc7ce0f863be59957d0962
Johnalexander1-arc <john.munoz04@uptc.edu.co>
2025-08-25 23:21:06 +0000

    tarea de algortimos, suma de 2 numeros, nombre y numero

---
+++ AlgoritmosTask1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+usinn namespace std;
+
+int main(){
+    string userName;
+    int userCode;
+    
+    int numberOne;
+    int numberTwo;
+
+    //Entrada por teclado
+    cout << "Escriba su nombre, por favor";
+    cin >> userName;
+
+    cout <<"Por favor, escriba su código estudiantil;"
+    cin >> userCode;
+    //Operacion matemática
+    cout <<"Por favor, ingrese un primer numero";
+    cin >> numberOne
+
+    cout <<"Por favor, ingrese un segundo numero";
+    cin >> numberTwo;
+
+    //salida poor teclado
+
+    cout <<"Hola: " <<userName <<endl;
+    cout <<" Su codigo estudiantil:" <<userCode "es valido";
+
+    cout <<"la suma de los 2 numeros que usted ingresó es: " <<numberOne + numberTwo <<endl;
+
+
+}
