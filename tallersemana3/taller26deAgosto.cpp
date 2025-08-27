0374b87708bc3077616473b1e7a6528e1aed5f81
Johnalexander1-arc <john.munoz04@uptc.edu.co>
2025-08-27 22:59:49 +0000

    calculadora con switch

--- tallersemana3/taller26deAgosto.cpp
+++ tallersemana3/taller26deAgosto.cpp
@@ -9,6 +9,7 @@ int main (){
     int resta;
     int prodcuto;
     int division;
+    int modulo;
     int numberOne;
     int numberTwo;
     int userSelection;
@@ -23,23 +24,46 @@ int main (){
     cin >> userSelection;
 
     if(userSelection ==1){
+        cout <<"ingrese el primer numero";
+        cin >> numberOne;
+        cout <<"ingrese el segundo numero";
+        cin >>numberTwo;
     cout<<"la suma de los 2 numeros es: " <<numberOne + numberTwo <<endl;
-
+        cin>>suma;
         
 
     } 
     else if(userSelection ==2) {
+        cout <<"ingrese el primer numero";
+        cin >> numberOne;
+        cout <<"ingrese el segundo numero";
+        cin >>numberTwo;
         cout <<"la resta de los 2 numeros es: " <<numberOne-numberTwo <<endl;
+        cin>> resta;
     }
     else if(userSelection==3){
+        cout <<"ingrese el primer numero";
+        cin >> numberOne;
+        cout <<"ingrese el segundo numero";
+        cin >>numberTwo;
         cout<<"el resultado de la multiplicacion es: " <<numberOne*numberTwo <<endl;
-    
+        cin >>prodcuto;
     }
     else if(userSelection==4){
+        cout <<"ingrese el primer numero";
+        cin >> numberOne;
+        cout <<"ingrese el segundo numero";
+        cin >>numberTwo;
         cout<<"el resultado de la division es: " <<numberOne/numberTwo <<endl;
+        cin>>division;
     }
     else if(userSelection==5){
+        cout <<"ingrese el primer numero";
+        cin >> numberOne;
+        cout <<"ingrese el segundo numero";
+        cin >>numberTwo;
         cout<<"el modulo es: " <<numberOne%numberTwo <<endl;
+        cin>> modulo;
     }
     return 0;
 
---
+++ tallersemana3/tarea28deAgosto.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int option;
+int numberOne;
+int numberTwo;
+string userName;
+
+
+//bienvenida
+
+cout <<"por favor ingrese su nombre: " <<endl;
+cin >>userName;
+cout <<" hola "<< userName ;
+cout <<" Bienvenido al programa, por favor seleciones 2 numeros y la operacion que desea realizar entre ellos: "  <<endl;
+
+cout <<"Elija la operacion a realizar: 1= suma, 2 =resta, 3 = producto, 4 = division , 5 = modulo  : ";
+cin >> option;
+//solicitar numeros
+cout <<"ingrese el primer numero: " <<endl;
+cin >>numberOne;
+cout <<"ingrese el segundo numero: " <<endl;
+cin >>numberTwo;
+
+
+//eleccion de opciones
+switch (option){
+   case 1:
+        cout <<" la suma de los 2 numeros es: " <<numberOne + numberTwo <<endl;
+        break;
+    case 2:
+        cout <<" la resta de los 2 numeros es: " <<numberOne - numberTwo <<endl;
+        break;
+    case 3:
+        cout <<" el producto de los 2 numeros es: " <<numberOne * numberTwo <<endl;
+        break;
+    case 4:
+        cout <<" la division de los 2 numeros es: " <<numberOne / numberTwo <<endl;
+        break;
+    case 5:
+        cout <<" el modulo de los 2 numeros es: " <<numberOne % numberTwo <<endl;
+        break;
+        default:
+        cout <<"la opcion no es valida : " <<endl;
+
+    }
+
+    return 0;
+}
